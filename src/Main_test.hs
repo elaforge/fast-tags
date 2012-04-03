9816a9f81b9d61888a858b16702bf594c508920c
qdunkan <qdunkan@gmail.com>
2012-04-02 20:58:42 -0700

    add awful ghc hack to get source line locations
    Ignore-this: 9c62e8503018635608d21c561eb0e4f6
    
    darcs-hash:20120403035842-2aa8e-f54f37cbf2117b64612efa057427b94137ce0f0b.gz

--- src/Main_test.hs
+++ src/Main_test.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main_test where
 import Control.Monad
+import qualified System.IO.Unsafe as Unsafe
 import qualified Data.Text as Text
+import qualified Control.Exception as Exception
+import Exception (assert)
 
 import qualified Main as Main
 import Main (Token, TokenVal(..), TagVal(..), Type(..))
 
 
--- This is kind of annoying without line numbers on equal and automatic
--- test_* collection...
+-- This is kind of annoying without automatic test_* collection...
 main = do
     test_tokenize
     test_breakString
@@ -18,89 +20,93 @@ main = do
 test_tokenize = do
     -- drop leading "nl 0"
     let f = drop 1 . extractTokens . tokenize
-    equal (f "a::b->c") ["a", "::", "b", "->", "c"]
-    equal (f "x{-\n  bc#-}\n")
+    equal assert (f "a::b->c") ["a", "::", "b", "->", "c"]
+    equal assert (f "x{-\n  bc#-}\n")
         ["x", "{-", "nl 2", "bc", "#", "-}"]
-    equal (f "X.Y") ["X.Y"]
-    equal (f "x9") ["x9"]
-    -- equal (f "9x") ["nl 0", "9", "x"]
-    equal (f "x :+: y") ["x", ":+:", "y"]
-    equal (f "(#$)") ["(#$)"]
-    equal (f "$#-- hi") ["$#", "--", "hi"]
-    equal (f "(*), (-)") ["(*)", ",", "(-)"]
+    equal assert (f "X.Y") ["X.Y"]
+    equal assert (f "x9") ["x9"]
+    -- equal assert (f "9x") ["nl 0", "9", "x"]
+    equal assert (f "x :+: y") ["x", ":+:", "y"]
+    equal assert (f "(#$)") ["(#$)"]
+    equal assert (f "$#-- hi") ["$#", "--", "hi"]
+    equal assert (f "(*), (-)") ["(*)", ",", "(-)"]
 
 test_breakString = do
     let f = Main.breakString
-    equal (f "hi \" there") ("hi \"", " there")
-    equal (f "hi \\a \" there") ("hi \\a \"", " there")
-    equal (f "hi \\\" there\"") ("hi \\\" there\"", "")
-    equal (f "hi") ("hi", "")
+    equal assert (f "hi \" there") ("hi \"", " there")
+    equal assert (f "hi \\a \" there") ("hi \\a \"", " there")
+    equal assert (f "hi \\\" there\"") ("hi \\\" there\"", "")
+    equal assert (f "hi") ("hi", "")
     -- String continuation isn't implemented yet.
-    equal (f "hi \\") ("hi \\", "")
+    equal assert (f "hi \\") ("hi \\", "")
 
 test_stripComments = do
     let f = extractTokens . Main.stripComments . tokenize
-    equal (f "hello -- there") ["nl 0", "hello"]
-    equal (f "hello {- there -} fred") ["nl 0", "hello", "fred"]
-    equal (f "{-# LANG #-} hello {- there {- nested -} comment -} fred")
+    equal assert (f "hello -- there") ["nl 0", "hello"]
+    equal assert (f "hello {- there -} fred") ["nl 0", "hello", "fred"]
+    equal assert (f "{-# LANG #-} hello {- there {- nested -} comment -} fred")
         ["nl 0", "hello", "fred"]
 
 test_breakBlocks = do
     let f = map extractTokens . map Main.stripNewlines
             . Main.breakBlocks . tokenize
-    equal (f "1\n2\n") [["1"], ["2"]]
-    equal (f "1\n 1\n2\n") [["1", "1"], ["2"]]
-    equal (f "1\n 1\n 1\n2\n") [["1", "1", "1"], ["2"]]
+    equal assert (f "1\n2\n") [["1"], ["2"]]
+    equal assert (f "1\n 1\n2\n") [["1", "1"], ["2"]]
+    equal assert (f "1\n 1\n 1\n2\n") [["1", "1", "1"], ["2"]]
     -- intervening blank lines are ignored
-    equal (f "1\n 1\n\n 1\n2\n") [["1", "1", "1"], ["2"]]
-    equal (f "1\n\n\n 1\n2\n") [["1", "1"], ["2"]]
+    equal assert (f "1\n 1\n\n 1\n2\n") [["1", "1", "1"], ["2"]]
+    equal assert (f "1\n\n\n 1\n2\n") [["1", "1"], ["2"]]
 
-    equal (f "1\n 11\n 11\n") [["1", "11", "11"]]
-    equal (f " 11\n 11\n") [["11"], ["11"]]
+    equal assert (f "1\n 11\n 11\n") [["1", "11", "11"]]
+    equal assert (f " 11\n 11\n") [["11"], ["11"]]
 
 test_process = test_misc >> test_data >> test_functions >> test_class
 
 test_misc = do
     let f = process
-    equal (f "module Bar.Foo where\n") [Tag "Foo" Module]
-    equal (f "newtype Foo a b =\n\tBar x y z\n")
+    equal assert (f "module Bar.Foo where\n") [Tag "Foo" Module]
+    equal assert (f "newtype Foo a b =\n\tBar x y z\n")
         [Tag "Foo" Type, Tag "Bar" Constructor]
-    equal (f "f :: A -> B\ng :: C -> D\ndata D = C {\n\tf :: A\n\t}\n")
+    equal assert (f "f :: A -> B\ng :: C -> D\ndata D = C {\n\tf :: A\n\t}\n")
         [Tag "f" Function, Tag "g" Function, Tag "D" Type,
             Tag "C" Constructor, Tag "f" Function]
 
 test_data = do
     let f = map untag . process
-    equal (f "data X\n") ["X"]
+    equal assert (f "data X\n") ["X"]
     -- The extra X is suppressed.
-    equal (f "data X = X Int\n") ["X"]
-    equal (f "data Foo = Bar | Baz") ["Foo", "Bar", "Baz"]
-    equal (f "data Foo =\n\tBar\n\t| Baz") ["Foo", "Bar", "Baz"]
+    equal assert (f "data X = X Int\n") ["X"]
+    equal assert (f "data Foo = Bar | Baz") ["Foo", "Bar", "Baz"]
+    equal assert (f "data Foo =\n\tBar\n\t| Baz") ["Foo", "Bar", "Baz"]
     -- Records.
-    equal (f "data Foo a = Bar { field :: Field }") ["Foo", "Bar", "field"]
-    equal (f "data R = R { a::X, b::Y }") ["R", "a", "b"]
-    equal (f "data R = R {\n\ta::X\n\t, b::Y\n\t}") ["R", "a", "b"]
-    equal (f "data R = R {\n\ta,b::X\n\t}") ["R", "a", "b"]
+    equal assert (f "data Foo a = Bar { field :: Field }")
+        ["Foo", "Bar", "field"]
+    equal assert (f "data R = R { a::X, b::Y }") ["R", "a", "b"]
+    equal assert (f "data R = R {\n\ta::X\n\t, b::Y\n\t}") ["R", "a", "b"]
+    equal assert (f "data R = R {\n\ta,b::X\n\t}") ["R", "a", "b"]
 
 test_functions = do
     let f = process
     -- Multiple declarations.
-    equal (f "a,b::X") [Tag "a" Function, Tag "b" Function]
+    equal assert (f "a,b::X") [Tag "a" Function, Tag "b" Function]
     -- With an operator.
-    equal (f "(+), a :: X") [Tag "+" Function, Tag "a" Function]
+    equal assert (f "(+), a :: X") [Tag "+" Function, Tag "a" Function]
     -- Don't get fooled by literals.
-    equal (f "1 :: Int") []
+    equal assert (f "1 :: Int") []
 
 test_class = do
     let f = map untag . process
-    equal (process "class (X x) => C a b where\n\tm :: a->b\n\tn :: c\n")
+    equal assert (process "class (X x) => C a b where\n\tm :: a->b\n\tn :: c\n")
         [Tag "C" Class, Tag "m" Function, Tag "n" Function]
-    equal (f "class A a where f :: X\n") ["A", "f"]
+    equal assert (f "class A a where f :: X\n") ["A", "f"]
     -- indented inside where
-    equal (f "class X where\n\ta, (+) :: X\n") ["X", "a", "+"]
-    equal (f "class X where\n\ta :: X\n\tb, c :: Y") ["X", "a", "b", "c"]
-    equal (f "class X\n\twhere\n\ta :: X\n\tb, c :: Y") ["X", "a", "b", "c"]
-    equal (f "class X\n\twhere\n\ta ::\n\t\tX\n\tb :: Y") ["X", "a", "b"]
+    equal assert (f "class X where\n\ta, (+) :: X\n") ["X", "a", "+"]
+    equal assert (f "class X where\n\ta :: X\n\tb, c :: Y")
+        ["X", "a", "b", "c"]
+    equal assert (f "class X\n\twhere\n\ta :: X\n\tb, c :: Y")
+        ["X", "a", "b", "c"]
+    equal assert (f "class X\n\twhere\n\ta ::\n\t\tX\n\tb :: Y")
+        ["X", "a", "b"]
 
 process :: Text.Text -> [TagVal]
 process = map Main.valOf . Main.process "fn"
@@ -120,6 +126,15 @@ extractTokens = map $ \token -> case Main.valOf token of
     Token name -> name
     Newline n -> Text.pack ("nl " ++ show n)
 
-equal :: (Show a, Eq a) => a -> a -> IO ()
-equal x y = unless (x == y) $
-    putStrLn $ "__: " ++ show x ++ " /= " ++ show y
+equal :: (Show a, Eq a) => Assert z -> a -> a -> IO ()
+equal srcpos x y = unless (x == y) $
+    putStrLn $ "__ " ++ getSourceLoc srcpos ++ " " ++ show x ++ " /= " ++ show y
+
+type Assert a = Bool -> a -> String
+
+-- | Awful ghc hack to get source line location.
+getSourceLoc :: Assert a -> String
+getSourceLoc assert_ = takeWhile (/=' ') $ Unsafe.unsafePerformIO $
+    Exception.evaluate (assert_ False (error "Impossible"))
+        `Exception.catch` (\(Exception.AssertionFailed s) -> return s)
+
