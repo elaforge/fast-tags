bcdeb042b2f32a75ab26b0c0399824f9dcdc140f
Sergey Vinokurov <serg.foo@gmail.com>
2019-10-09 21:36:03 +0100

    Review suggestions
    - Add more docs
    - Use bytestring's accursedUnutterablePerformIO instead of rolling our own
    - Move some functions within FastTags.LexerM around to group related ones together

--- src/FastTags/LexerTypes.hs
+++ src/FastTags/LexerTypes.hs
@@ -2,14 +2,14 @@
 -- |
 -- Module      :  FastTags.LexerTypes
 -- Copyright   :  (c) Sergey Vinokurov 2019
+--
+-- Auxiliary types for lexer.
 ----------------------------------------------------------------------------
 
 {-# LANGUAGE BangPatterns               #-}
 {-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
-
-{-# OPTIONS_GHC -O2 #-}
 
 module FastTags.LexerTypes
     ( Context(..)
@@ -23,6 +23,8 @@ module FastTags.LexerTypes
 
 import Control.DeepSeq
 
+-- | Whether we're currently in Haskell context or within Template Haskell
+-- quasiquoter and should discard as much as possible.
 data Context = CtxHaskell | CtxQuasiquoter
     deriving (Show, Eq, Ord)
 
@@ -30,9 +32,13 @@ data Context = CtxHaskell | CtxQuasiquoter
 newtype AlexCode = AlexCode { unAlexCode :: Int }
     deriving (Eq, Ord, Show, Enum, Num, Real, Integral, NFData)
 
+-- | Literate style we're currently in.
 data LitStyle = Bird | Latex
     deriving (Eq, Ord, Show, Enum, Bounded)
 
+-- | Literate mode of whole file. Either @LitVanilla@ all the time, which means
+-- it's non-literate file or @LitOutside@ when looking at literate comments and
+-- @LitInside mode@ when lexing Haskell source with specific literate @mode@.
 data LitMode a = LitInside !a | LitOutside | LitVanilla
     deriving (Eq, Ord, Show, Functor)
 
